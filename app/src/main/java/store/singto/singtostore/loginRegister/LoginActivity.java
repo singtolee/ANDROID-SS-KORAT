6a708ada6de40657f520782ddc87667e46752a14
QIANG LI <qiangli@QIANGs-Mac-mini.local>
2017-11-09 06:37:40 +0700

    facebook login and firebase set up.

---
+++ app/src/main/java/store/singto/singtostore/loginRegister/LoginActivity.java
@@ -0,0 +1,16 @@
+package store.singto.singtostore.loginRegister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import store.singto.singtostore.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
