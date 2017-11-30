8962fc84312bcc81a3ff8ebb9ef507c8ab7570b3
QIANG LI <qiangli@QIANGs-Mac-mini.local>
2017-12-01 00:04:06 +0700

    able to load all categories , populate category tabs

---
+++ app/src/main/java/store/singto/singtostore/AllProductsTab/Category.java
@@ -0,0 +1,12 @@
+package store.singto.singtostore.AllProductsTab;
+
+/**
+ * Created by qiangli on 11/30/2017 AD.
+ */
+
+public class Category {
+    public String id;
+    public String TH;
+    public String ENG;
+
+}
--- app/src/main/java/store/singto/singtostore/AllProductsTab/TabAllProductsFragment.java
+++ app/src/main/java/store/singto/singtostore/AllProductsTab/TabAllProductsFragment.java
@@ -1,107 +1,131 @@
 package store.singto.singtostore.AllProductsTab;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import store.singto.singtostore.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TabAllProductsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TabAllProductsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TabAllProductsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TabLayout tabbar;
+    private ViewPager viewPager;
 
-    private OnFragmentInteractionListener mListener;
+    private List<Category> categories;
+
+    private FirebaseFirestore db;
+
+
 
     public TabAllProductsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TabAllProductsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TabAllProductsFragment newInstance(String param1, String param2) {
-        TabAllProductsFragment fragment = new TabAllProductsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_all_products, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_tab_all_products, container, false);
+
+        viewPager = view.findViewById(R.id.categoryfragmentsviewpager);
+        tabbar = view.findViewById(R.id.categorytabbar);
+
+        viewPager.setOffscreenPageLimit(3);
+        tabbar.setupWithViewPager(viewPager);
+
+        categories = new ArrayList<>();
+
+        db = FirebaseFirestore.getInstance();
+
+
+        loadcategories();
+
+        return view;
+
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void loadcategories(){
+        db.collection("CATEGORIES").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    Adapter adapter = new Adapter(getFragmentManager());
+                    for(DocumentSnapshot documentSnapshot : task.getResult()){
+                        Category category = documentSnapshot.toObject(Category.class);
+                        categories.add(category);
+                        CategoryprdFragment categoryprdFragment = new CategoryprdFragment();
+                        adapter.addFragment(categoryprdFragment, category.ENG);
+                    }
+                    viewPager.setAdapter(adapter);
+                }
+            }
+        });
     }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
 }
